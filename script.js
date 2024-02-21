a2ec8c9c08fb5550f4a1922a4028bfffb2b370e4
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2024-02-21 21:22:49 +0200

    Проверка строки

--- script.js
+++ script.js
@@ -29,8 +29,7 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}
-${tg.version}`;
+p.innerText = `Здравствуйте, ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name} !
+Версия API: ${tg.version}`;
 
 usercard.appendChild(p);
