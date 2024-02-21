20fe68bac5d51d7e94ba3eb1224e2a6247284d13
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2024-02-21 19:52:00 +0200

    123

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+${tg.initDataUnsafe.user.last_name}
+${tg.version}`;
 
 usercard.appendChild(p);
