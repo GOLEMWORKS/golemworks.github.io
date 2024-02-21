12434de127508c2a26fe0fa050afe5a8a42456bf
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2024-02-21 21:58:31 +0200

    Кнопки

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ tg.MainButton.color = "#2cab37";
 let item = "";
 
 let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+let btn3 = document.getElementById("btn3")
+let btn4 = document.getElementById("btn4")
 
 btn1.addEventListener("click", function(){
     if(tg.MainButton.isVisible){
@@ -14,6 +17,36 @@ btn1.addEventListener("click", function(){
     } else {
         tg.MainButton.setText(`Вы выбрали товар ${item}!`);
         item = "Кофе";
+        tg.MainButton.show();
+    }
+})
+
+btn2.addEventListener("click", function(){
+    if(tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText(`Вы выбрали товар ${item}!`);
+        item = "Вискас мурчикам";
+        tg.MainButton.show();
+    }
+})
+
+btn3.addEventListener("click", function(){
+    if(tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText(`Найти ближайщий ${item}!`);
+        item = "Отель";
+        tg.MainButton.show();
+    }
+})
+
+btn4.addEventListener("click", function(){
+    if(tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText(`Вы решили стать самым модным ${item}!`);
+        item = "";
         tg.MainButton.show();
     }
 })
